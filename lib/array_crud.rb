b5990c0202e3e98dca8ea8c55d8be8e4f806c886
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 18:33:35 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,66 @@
 def create_an_empty_array
   
+  empty_array = []
+  
+  return empty_array
+  
 end
 
 def create_an_array
   
+  led_zepplin = ['Jimmy Page', 'Robert Plant', 'John Paul Jones', 'John Bonham' ]
+  
+  return led_zepplin
+  
 end
 
 def add_element_to_end_of_array(array, element)
+
+  
+  array.push(element)
+  
+  return array
   
 end
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift(element)
+  
+  return array
+  
 end
 
 def remove_element_from_end_of_array(array)
   
+ new_array =  array.pop
+  
+  return new_array 
+
 end
 
 def remove_element_from_start_of_array(array)
+  
+  new_array = array.shift()
+  
+  return new_array 
   
 end
 
 def retrieve_element_from_index(array, index_number)
   
+ return array[index_number]
+  
+  
 end
 
 def retrieve_first_element_from_array(array)
+  
+  return array[0]
   
 end
 
 def retrieve_last_element_from_array(array)
   
+  return array[-1]
 end
